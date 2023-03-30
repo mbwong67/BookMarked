97e55426c19895e2496b28dfdeb30c1ec619978c
wyamet <wyattdomanski.md@gmail.com>
2023-03-30 11:07:34 -0700

    updated server.js file to handle graphQL

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-db.once('open', () => {
+server.applyMiddleware({ app });
+
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
