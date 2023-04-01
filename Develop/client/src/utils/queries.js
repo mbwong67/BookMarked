417a8a2e0696b3d4c8ed0b79cc02e839657aaf16
wyamet <wyattdomanski.md@gmail.com>
2023-04-01 16:23:43 -0700

    added REMOVE_BOOK export to queries.js

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -18,3 +18,22 @@ export const GET_ME = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
